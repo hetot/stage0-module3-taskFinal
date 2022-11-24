c513fffbd6c93c3624878a3a80c9360941171d2a
hetot <shaxzod.murtazayev@mail.ru>
2022-11-24 20:10:57 +0300

    done

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        System.out.println(1.8 * temperatureCelsius + 32);
     }
 }
